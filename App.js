11c6f51ae633ee4a9b9089c282aa6d0d95baf60c
lovecoding-git <fedde198212@gmail.com>
2021-08-28 02:40:27 +0800

    scheduled push notification

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect} from 'react';
+import {Button, View} from 'react-native';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import { Audio } from 'expo-av';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  
+  const [sound, setSound] = React.useState();
+  
+  useEffect(()=>{
+    registerNotification().then(()=>{
+      console.log("success registering notification")
+    });
+  },[])
+  
+  async function playSound() {
+    
+    console.log('Loading Sound');
+    
+    const { sound } = await Audio.Sound.createAsync(
+        require('./assets/ring.wav'),
+        {
+          shouldPlay: true,
+          isLooping: true,
+        },
+        null,
+        true
+    );
+    
+    setSound(sound);
+    
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  useEffect(() => {
+    return sound
+        ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+        : undefined;
+  }, [sound]);
+  
+  
+  const onSubmit = () => {
+    Notifications.scheduleNotificationAsync ({
+      content: {
+        title: "Time's up!",
+        body: 'Change sides!',
+        sound: true
+      },
+      trigger: {
+        seconds: 2,
+      },
+    }).then(r => {
+      console.log(r)
+    });
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const handleNotification = async () => {
+     // await playSound();
+  }
+  
+  const registerNotification = async () => {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+    Notifications.addNotificationReceivedListener(handleNotification);
+  }
+
+  return (
+      <View style={{flex: 1, alignItems:'center', justifyContent: 'center', width:'100%'}}>
+        <Button onPress={onSubmit} title={'Submit'}/>
+      </View>
+  );
+  
+}
